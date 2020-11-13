ee728aafa5c7917e73193f78ca620935a8fd49db
FranBesq <franciscobesq@gmail.com>
2020-11-13 20:35:25 +0100

    Fixed adding random noise to img

--- attackEnvTest.py
+++ attackEnvTest.py
@@ -5,38 +5,51 @@ import numpy as np
 from PIL import Image
 
 #Reduces img to desired size
-def image_preporcess(image, target_size):
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
 
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
 
-    ih, iw    = target_size
-    h,  w, _  = image.shape
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
 
-    scale = min(iw/w, ih/h)
-    nw, nh  = int(scale * w), int(scale * h)
-    image_resized = cv2.resize(image, (nw, nh))
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
 
-    image_paded = np.full(shape=[ih, iw, 3], fill_value=128.0)
-    dw, dh = (iw - nw) // 2, (ih-nh) // 2
-    image_paded[dh:nh+dh, dw:nw+dw, :] = image_resized
-    image_paded = image_paded / 255.
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
 
-    return image_paded
-
-
-img_path = "senal-stop.jpg"
+    # return the resized image
+    return resized
 
 
+img_path = "img/senal-stop-416.jpg"
 adv_size = 64 #Size of adversarial image
-
 
 #Load target image
 img_original = cv2.imread(img_path)
-img_original = cv2.cvtColor(img_original, cv2.COLOR_BGR2RGB)
-#Shape original image to fit yolo input size
-img_mod = image_preporcess(np.copy(img_original), [416, 416])
+#img_original = cv2.cvtColor(img_original, cv2.COLOR_BGR2RGB)
 
-cv2.imwrite('senal-stop-416.jpg', img_mod)
+#Shape original image to fit yolo input size
+#img_mod = image_resize(img_original, 416, 416)
+#img_mod = cv2.resize(img_original, (416, 416))
+#img_mod.astype(np.uint8)
+
+#cv2.imwrite('senal-stop-416.jpg', img_mod)
 
 #Create random image
 img_adv = np.ones((adv_size, adv_size, 3), dtype=np.uint8)
@@ -46,19 +59,15 @@ cv2.randu(bgr[1], 0, 255)
 cv2.randu(bgr[2], 0, 255)
 img_adv = cv2.merge(bgr)
 
-cv2.imwrite('adv_img.jpg', img_adv)
-img_adv = cv2.imread('adv_img.jpg')
-
-#alpha_adv = 1
-#alpha_or = 1
-
-#for c in range(0, 3):
-#    img_mod[250:250+adv_size, 200:200+adv_size, c] = (alpha_adv * img_adv[:, :, c] + alpha_or * img_mod[250:250+adv_size, 200:200+adv_size, c])
+cv2.imwrite('img/adv_img.jpg', img_adv)
+img_adv = cv2.imread('img/adv_img.jpg')
 
 #Show results
 #img_adv = Image.fromarray(img_adv)
 
-cv2.imshow("Composited image", img_mod)
-#cv2.imwrite('result.jpg', img_mod)
+img_original[150:150+adv_size, 150:150+adv_size] = img_adv
+
+cv2.imshow("Composited image", img_original)
+cv2.imwrite('result.jpg', img_original)
 cv2.waitKey(0)
 
